31e9244f992191b71ab42eff0f1542b0835d5360
stephenyau <softwareentrepreneer@gmail.com>
2024-04-18 17:23:55 +0800

    docs: theme update

--- docs/build.sh
+++ docs/build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+CONFIG_PATH="docs/_config.yml"
+
+# echo "Google Analytics ID: $GA_TRACKING_ID"
+# echo "Current Directory: $(pwd)"
+
+# echo "Before replacement:"
+# grep "google_analytics_id" $CONFIG_PATH
+
 # Replace the placeholder in the YAML file with the actual Google Analytics ID
-sed -i "s/{GA_TRACKING_ID_PLACEHOLDER}/$GA_TRACKING_ID/" _config.yml
+sed -i "s/{GA_TRACKING_ID_PLACEHOLDER}/$GA_TRACKING_ID/" $CONFIG_PATH
+
+# echo "After replacement:"
+# grep "google_analytics_id" $CONFIG_PATH
 
 # Build
 jupyter-book build docs/ --all
