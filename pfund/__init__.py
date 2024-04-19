c0dd54641717f60bb91febbdb820747a21f06555
stephenyau <softwareentrepreneer@gmail.com>
2024-04-19 15:01:43 +0800

    fix: click cli latency

--- pfund/__init__.py
+++ pfund/__init__.py
@@ -1,32 +1,70 @@
 import sys
 from importlib.metadata import version
 
+from pfund.config_handler import configure
+from pfund.utils.aliases import ALIASES
 from pfund.const.paths import PROJ_PATH
 # add python path so that for files like "ibapi" (official python code from IB) can find their modules
 sys.path.append(f'{PROJ_PATH}/externals')
-from pfund.config_handler import configure
-from pfund.engines import BacktestEngine, TrainEngine, TestEngine, TradeEngine
-from pfund.strategies import Strategy
-from pfund.models import Feature, Model
-from pfund.utils.aliases import ALIASES
-try:
-    from pfund.models import PyTorchModel
-except ImportError:
-    pass
-try:
-    from pfund.models import SKLearnModel
-except ImportError:
-    pass
-from pfund.indicators import TAIndicator, TALibIndicator
+
+
+# NOTE: dynamically import modules to avoid click cli latency (reduced from ~4s to ~0.2s)
+def __getattr__(name):
+    """
+    Dynamically import and return modules and classes based on their name.
+    
+    Supports dynamic loading of data sources and feed classes to minimize
+    initial load time.
+    """
+    import importlib
+    if 'Engine' in name:
+        Engine = getattr(importlib.import_module('pfund.engines'), name)
+        globals()[name] = Engine
+        return Engine
+    elif 'Strategy' in name:
+        Strategy = getattr(importlib.import_module('pfund.strategies'), name)
+        globals()[name] = Strategy
+        return Strategy
+    elif 'Model' in name or 'Feature' in name:
+        Model = getattr(importlib.import_module('pfund.models'), name)
+        globals()[name] = Model
+        return Model
+    elif 'Indicator' in name:
+        Indicator = getattr(importlib.import_module('pfund.indicators'), name)
+        globals()[name] = Indicator
+        return Indicator
+
+
+# NOTE: dummy classes/modules for type hinting
+# e.g. import pfund as pf, when you type "pf.", 
+# you will still see the following suggestions even they are dynamically imported:
+BacktestEngine: ...
+TradeEngine: ...
+TrainEngine: ...
+TestEngine: ...
+Strategy: ...
+Model: ...
+PyTorchModel: ...
+SKLearnModel: ...
+Feature: ...
+TAIndicator: ...
+TALibIndicator: ...
 
 
 __version__ = version('pfund')
-
-
 __all__ = (
     '__version__',
-    'configure', 'ALIASES',
-    'BacktestEngine', 'TrainEngine', 'TestEngine', 'TradeEngine',
-    'Strategy', 'Model', 'PyTorchModel', 'SKLearnModel',
-    'Feature', 'TAIndicator', 'TALibIndicator',
+    'configure', 
+    'ALIASES',
+    'BacktestEngine', 
+    'TradeEngine',
+    'TrainEngine', 
+    'TestEngine',
+    'Strategy', 
+    'Model', 
+    'PyTorchModel', 
+    'SKLearnModel',
+    'Feature', 
+    'TAIndicator', 
+    'TALibIndicator',
 )
--- pfund/main.py
+++ pfund/main.py
@@ -1,8 +1,16 @@
+import atexit
+
 from pfund.cli import pfund_group
+
+
+def exit_cli():
+    """Application Exitpoint."""
+    print("Cleanup actions here...")
 
 
 def run_cli() -> None:
     """Application Entrypoint."""
+    # atexit.register(exit_cli)
     pfund_group(obj={})
 
 
